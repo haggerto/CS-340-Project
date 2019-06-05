04f4986e16a8a3c9ef6c33f6ec23152471eb1724
XIZHI YANG <35322574+yangxizhi@users.noreply.github.com>
2019-06-05 14:40:31 -0700

    Create pages.php
    try working php out

---
+++ PHP/pages.php
@@ -0,0 +1,8 @@
+<?php
+	$content = array(
+		"Home Page" => "home.php",
+		"Profucts" => "products.php",
+		"On Sale" => "on-sale.php",
+		"Account" => "account.php",
+    "Information" => "information.php");
+?>
