de0b1556c78613676c0b2a21cfcb750ea9f0c6f9
brgoodlett <brgoodlett@gmail.com>
2019-06-08 19:31:19 -0700

    shopping cart
    Not sure why this isn't working. The query should be working, but it always returns false.

--- js/home.js
+++ js/home.js
@@ -1,5 +1,9 @@
 /*Adds a product to the current user's cart
   Takes the id of the product*/
 function addToCart(id){
-	console.log(id);
+
+  var request = new XMLHttpRequest();
+	var requestUrl = "updateCart.php?type=add&itemID="+id+"&userID="+getCurrentUser();
+  request.open("POST", requestUrl, true);
+  request.send(null);
 }
---
+++ php/updateCart.php
@@ -0,0 +1,39 @@
+<?php
+	include 'dbconnect.php';
+	$connection = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+	if(!$connection){	//confirm that the connection was made
+		die("DB_CONNECTION_FAILED");
+	}
+
+	session_start();
+	
+	if(!isset($_SESSION['user'])){	//confirm that a user is logged in
+		die("NO_USER");
+	}
+	
+	$getOp = $_GET["type"];
+	switch($getOp){
+		case add:
+				//check if the user has an existing order
+				$existingOrderQuery = sprintf('SELECT * FROM Orders WHERE Orders.userID = %d AND Orders.orderStatus = %s', $_SESSION['user'],'shop');
+				echo $existingOrderQuery;
+				$existingOrderResult = mysqli_query($connection, $existingOrderQuery);
+
+				if(mysql_num_rows($existingOrderResult) == 0) {
+					$query = sprintf('INSERT INTO Orders(userID, totalCost, shippingAddress, orderStatus, totalWeight, shippingZip) VALUES( %d, 0, \'none\', shop, 0, 00000)', $_SESSION['user']);
+					mysqli_query($connection, $query);
+					echo "FALSE";
+				} else {
+					echo "TRUE";
+				}
+				//if they do, check if the item is already in the orderContents with that orderID
+				//if it is, increment the count
+				//if it is not, add it with a count of 1
+				//if they do not already have an order, create one and an orderContents
+				//add the productID to the ordercontents.
+			break;
+		default:
+			echo "UNKNOWN_OPERATION";
+			break;
+	}
+?>
